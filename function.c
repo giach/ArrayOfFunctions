31277dc961ecc609b55c4b295420862e51d807e1
Georgiana Chelu <georgiana.chelu93@gmail.com>
2013-09-19 21:11:18 +0300

    Create function.c

---
+++ function.c
@@ -0,0 +1,26 @@
+#include "functie.h"
+
+int addTen(int i) {
+        return i + 10;
+}
+int subtractTen(int i) {
+        return i - 10;
+}
+int multiplyByTen(int i) {
+        return i * 10;
+}
+int divideByTen(int i) {
+        return i / 10;
+}
+int addEight(int i) {
+        return i + 8;
+}
+int addFive(int i) {
+        return i + 5;
+}
+int subtractFive(int i) {
+        return i - 5;
+}
+int divideByTwo(int i) {
+        return i / 2;
+}
