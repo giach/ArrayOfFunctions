dbff8187e5864e4ab169957ede3a8196e0a00243
Georgiana Chelu <georgiana.chelu93@gmail.com>
2013-09-19 21:12:36 +0300

    Create function.h

---
+++ function.h
@@ -0,0 +1,16 @@
+#ifndef __FUNC_H
+#define __FUNC_H
+
+typedef int (*Functions)(int);
+
+//Prototipurile functiilor date exemplu
+int addTen(int i);
+int subtractTen(int i);
+int multiplyByTen(int i);
+int divideByTen(int i);
+int addEight(int i);
+int addFive(int i);
+int subtractFive(int i);
+int divideByTwo(int i);
+
+#endif
