557a388c358fb91a438e3cd58f948c4c7876bea2
Georgiana Chelu <georgiana.chelu93@gmail.com>
2013-09-19 21:45:31 +0300

    Update function.c

--- function.c
+++ function.c
@@ -1,26 +1,26 @@
 #include "function.h"
 
 int addTen(int i) {
-        return i + 10;
+	return i + 10;
 }
 int subtractTen(int i) {
-        return i - 10;
+	return i - 10;
 }
 int multiplyByTen(int i) {
-        return i * 10;
+	return i * 10;
 }
 int divideByTen(int i) {
-        return i / 10;
+	return i / 10;
 }
 int addEight(int i) {
-        return i + 8;
+	return i + 8;
 }
 int addFive(int i) {
-        return i + 5;
+	return i + 5;
 }
 int subtractFive(int i) {
-        return i - 5;
+	return i - 5;
 }
 int divideByTwo(int i) {
-        return i / 2;
+	return i / 2;
 }
